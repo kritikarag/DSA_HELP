f60e4aef26f32b2379a73c99225851ba6d64b851
Kritika Rag <kritikarag.kr@gmail.com>
2023-04-08 13:18:32 +0530

    modified:   is_possible.cpp

--- is_possible.cpp
+++ is_possible.cpp
@@ -25,7 +25,7 @@ int main(){
             break;
         }
 
-        if(curr.x>c && curr.y>d)break;
+        if(curr.x>c && curr.y>d)break; // Try using || operator if it doesn't works
 
         pos temp1, temp2;
         temp1.x = curr.x + curr.y;
@@ -45,3 +45,56 @@ int main(){
 
     return 0;
 }
+
+
+
+
+
+#include <iostream>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+const int INF = 100000000;
+
+bool move(int a, int b, int c, int d, vector<vector<int>> &dp)
+{
+    // memoizing is here
+    if (dp[a][b] != -1)
+    {
+        return dp[a][b];
+    }
+    dp[a][b] = INF;
+    if (a == c && b == d)
+    {
+        return true;
+    }
+    else if (a > c || b > d)
+    {
+        return false;
+    }
+    else
+    {
+        bool ans = false;
+        if (move(a + b, b, c, d, dp))
+        {
+            ans = true;
+        }
+        if (move(a, a + b, c, d, dp))
+        {
+            ans = true;
+        }
+        dp[a][b] = ans;
+    }
+    return dp[a][b];
+}
+
+int main()
+{
+    int n = 1001, m = 1001;
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+    int a = 1, b = 2, c = 3, d = 6;
+    bool ans = move(a, b, c, d, dp);
+    cout << ans << endl;
+    return 0;
+} 
