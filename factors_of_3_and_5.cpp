f2f9c9ad5e744a7192b130f4eea19771c8c6f855
Kritika Rag <kritikarag.kr@gmail.com>
2023-04-08 14:39:03 +0530

    modified:   factors_of_3_and_5.cpp

--- factors_of_3_and_5.cpp
+++ factors_of_3_and_5.cpp
@@ -4,21 +4,20 @@ using namespace std;
 int main(){
     long high, low;
     cin>>low>>high;
-    vector<long>store1,store2,ans;
+    vector<long>store1,store2;
     long three = 1, five =1, res=0;
     while(three<=high){
         long five = 1;
         while(five<=high){
             int curr = three*five;
             if (curr >= low && curr <= high)
-            {
-                cout<<curr<<" ";
+            { 
                 res++;
             }
             five*=5;
         }
         three*=3;
     } 
-    //cout<<endl;
+    cout<<res<<endl;
     return 0;
 }
